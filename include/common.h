b422dc676f07b4e6fbfb3734a27a5292352af1ab
abosiljcic <96130877+abosiljcic@users.noreply.github.com>
2022-09-19 20:16:09 +0200

    finished

---
+++ include/common.h
@@ -0,0 +1,17 @@
+
+
+#ifndef PROJECT_BASE_COMMON_H
+#define PROJECT_BASE_COMMON_H
+#include <string>
+#include <fstream>
+#include <sstream>
+
+std::string readFileContents(std::string path) {
+    std::ifstream in(path);
+    std::stringstream buffer;
+    buffer << in.rdbuf();
+    return buffer.str();
+}
+
+
+#endif //PROJECT_BASE_COMMON_H
